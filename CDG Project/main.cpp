e92bf0f3254837ddced143d532f17ab0f9ccf7f1
Shafeef Omar <shafeef901@gmail.com>
2019-04-23 12:07:40 +0530

    Initial Commit

---
+++ CDG Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CDG Project
+//
+//  Created by Shafeef Omar on 23/04/19.
+//  Copyright © 2019 Shafeef Omar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
